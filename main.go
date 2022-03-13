5d181ac9d4f5816078e237f76136af07177d4652
Nathan Hanna <jnathanh@gmail.com>
2022-03-13 14:58:10 -0700

    add error when not invoked via go generate directive

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
+	"os"
 
 	"github.com/jnathanh/go-generate-cli/lib"
 )
@@ -12,6 +13,11 @@ func main() {
 	flag.String("testsignature", "", "flag for testing, does nothing except not throw an error when a value is passed with it")
 	pathArg := flag.String("path", "main.go", "specifies which path the generated main.go file should be written to")
 	flag.Parse()
+
+	if l := os.Getenv("GOLINE"); l == "" {
+		fmt.Println("currently supported only via invocation by `go generate`\nadd a `//go:generate go-generate-cli` line above the function you wish to be the model for the command line interface you want to generate")
+		os.Exit(1)
+	}
 
 	err := lib.Exec(lib.ExecOptions{Path: *pathArg})
 	if err != nil {
